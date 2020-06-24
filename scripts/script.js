3725d5cab772d1124dac5942d7e00b003cf44584
Peter Sterckx <pasterckx@gmail.com>
2020-06-23 20:55:59 -0400

    gallery trial

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,6 @@
-function myFunction(id) {
-    var x = document.getElementById(id);
-    if (x.className.indexOf("w3-show") == -1) {
-      x.className += " w3-show";
-    } else {
-      x.className = x.className.replace(" w3-show", "");
-    }
-  }
+var images = ['mountains.JPG', 'metz3.jpeg'];
+    
+// $('<img src="images/' + images[Math.floor(Math.random() * images.length)] + '">').appendTo('#banner-load');
 
-// function change(id) {
-//     var elem = document.getElementById(id);
-//     if (elem.innerHTML=="<i class='fas fa-plus'></i> Read more") elem.innerHTML = "<i class='fas fa-minus'></i> Read less";
-//     else elem.innerHTML = "<i class='fas fa-plus'></i> Read more";
-// }
-
-function change(id) {
-    var elem = document.getElementById(id);
-    if (elem.innerHTML.includes("more")) elem.innerHTML = "<i class='fas fa-minus'></i> Read less";
-    else elem.innerHTML = "<i class='fas fa-plus'></i> Read more";
-}
+$('<img src="images/metz3.jpeg">').appendTo('#banner-load');
 
