9041fa4196278b6601c2e58645f36773af19c9e6
Peter Sterckx <pasterckx@gmail.com>
2020-06-24 01:50:59 -0400

    gallery

--- scripts/script.js
+++
@@ -1,6 +0,0 @@
-var images = ['mountains.JPG', 'metz3.jpeg'];
-    
-// $('<img src="images/' + images[Math.floor(Math.random() * images.length)] + '">').appendTo('#banner-load');
-
-$('<img src="images/metz3.jpeg">').appendTo('#banner-load');
-
